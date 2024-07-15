b5201be384ac08b77e11d748c905ca192285a915
Christina De Cesaris <38991630+cmdecesaris@users.noreply.github.com>
2024-07-15 15:59:27 -0700

    doc edits

--- data-raw/sample_collections.R
+++ data-raw/sample_collections.R
@@ -1,10 +1,14 @@
 ## code to prepare `sample_collections` dataset goes here
-
-collection = getArthroCollections(getToken(),2020,2023)
-sample_collections = collection[c(1,3,7,9,10,11,17,19,23,28)]
+collection = getArthroCollections(getToken(),2019,2023, 'mosquito', 55)
+sample_collections = collection[c(1,3,7,8,9,10,11,17,19,23,28)]
 sample_collections$collection_id = sample_collections$collection_id-30050
 sample_collections$surv_year = sample_collections$surv_year-3
-write.csv(sample_collections,"data-raw/sample_collections.csv")
+sample_collections$collection_date = as.Date(ymd_hms(sample_collections$collection_date)-years(3))
+sample_collections = na.omit(sample_collections)
+#freq_t = data.frame(table(sample_collections$species_display_name)/dim(sample_collections)[1])
+#sample_collections$species_display_name = sample(freq_t$Var1,size=dim(sample_collections)[1],replace = T, prob=freq_t$Freq)
+
+write.csv(sample_collections,"data-raw/sample_collections.csv",append = F)
 
 sample_collections = read.csv("data-raw/sample_collections.csv", header = TRUE)
 sample_collections=sample_collections[-c(1)]
--- data-raw/sample_pools.R
+++ data-raw/sample_pools.R
@@ -1,10 +1,9 @@
 ## code to prepare `sample_pools` dataset
-
-pools = getPools(getToken(),2020,2023)
-sample_pools = pools[c(1,6,8,9,10,24,28,38,43)]
+pools = getPools(getToken(),2020,2023,'mosquito', 55)
+sample_pools = pools[c(1,6,8,9,10,24,28,38,44)]
 sample_pools$pool_id = sample_pools$pool_id-30050
 sample_pools$surv_year= sample_pools$surv_year-3
-
+sample_pools$collection_date = as.Date(ymd_hms(sample_pools$collection_date)-years(3))
 write.csv(sample_pools,"data-raw/sample_pools.csv")
 
 sample_pools = read.csv("data-raw/sample_pools.csv")
